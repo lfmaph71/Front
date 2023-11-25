b68c1115dd53d72ff4303564c4775ad602cb99c7
Leonardo Martinez <lfmaph71@users.noreply.github.com>
2023-11-25 09:59:39 -0500

    cambios

--- src/componentes/eliminarUsuario.jsx
+++ src/componentes/eliminarUsuario.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react';
+import { useParams,Link } from 'react-router-dom';
+import alerta, { sendRequest } from './funciones';
 
 const eliminarUsuario = () => {
+  let {idUsuario} = useParams();
+
+  useEffect(()=>{
+    eliminarUsuario();
+  },[])
+
+const eliminarUsuario = async()=>{
+  alerta("Barrar Usuario","warning");
+  let res = await sendRequest("GET",{},`/api/Usuarios/EliminarUsuarios/${idUsuario}`,'');
+  window.location.href = '/index';
+}
+
   return (
     <div>eliminarUsuario</div>
   )
